079d3f360c25f7d9b8940eac81f14cf7ec1e26f4
Ben Nicholas <ben@ben-nicholas.com>
2012-03-14 12:30:58 -0400

    Cleaned up a little to make things slightly easier

--- songs_db.rb
+++ songs_db.rb
@@ -1,6 +1,7 @@
-require "rubygems"
+# This ensures than any further requires refer to the version of the gems that you specify in your Gemfile
 require "bundler/setup"
 
+# Use sinatra
 require 'sinatra'
 
 get '/' do
